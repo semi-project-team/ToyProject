0737db76ade04c3f99301269f48902bdf9253166
wjdals83 <tjwjdals0803@naver.com>
2024-04-23 14:25:25 +0900

    급여로 조회하기

--- EmployeeToyProject/src/main/java/com/ohgiraffers/employeetoyproject/model/dao/JungminMapper.java
+++ EmployeeToyProject/src/main/java/com/ohgiraffers/employeetoyproject/model/dao/JungminMapper.java
@@ -4,10 +4,12 @@ import com.ohgiraffers.employeetoyproject.model.dto.EmployeeDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface JungminMapper {
 
     List<EmployeeDTO> findAllMenus();
 
+    List<EmployeeDTO> findBySalry(Map<String,Integer> param);
 }
